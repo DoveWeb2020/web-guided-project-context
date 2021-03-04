26feba88b5df159a9519c4263d5ff9ba704282ca
Josh Jacobson <joshjacobson14@gmail.com>
2021-03-03 16:57:11 -0800

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { createContext } from 'react';
 
 const App = () => {
     const [person, setPerson] = useState();
+
     return(<h1>Hello World</h1>);
 };
 
