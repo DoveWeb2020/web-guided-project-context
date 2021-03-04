ca777109586b2969b0331eb547709408433c46b2
Josh Jacobson <joshjacobson14@gmail.com>
2021-03-03 17:06:38 -0800

    lecture work-in-progress

--- review/src/Hello.js
+++ review/src/Hello.js
@@ -2,9 +2,6 @@ import React, { useContext } from 'react';
 import { PersonContext } from './AppContextState';
 
 const Hello = () => {
-  const [person, setPerson] = useState();
-  console.log("person context:", PersonContext);
-
   return (
     <h1>Hello, WEBPT24!</h1>
   );
