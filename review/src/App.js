fac9416a75a15973074aa99df7640f1ceece61c8
Josh Jacobson <joshjacobson14@gmail.com>
2021-03-03 17:09:44 -0800

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
     const [person, setPerson] = useState("Joshhhhh");
     console.log("person context:", PersonContext);
     
-    return (<PersonContext.Provider value={person}>
+    return (<PersonContext.Provider value={personValue: {person, setPerson}}>
         <Hello/>
     </PersonContext.Provider>);
 };
